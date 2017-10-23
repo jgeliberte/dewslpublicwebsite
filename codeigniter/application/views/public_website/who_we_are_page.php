8d8d139b46fc4e044b03460e5bd8a4bc603e8848
John Geliberte <gelibertejohn@gmail.com>
2017-10-23 12:03:16 +0800

    Finalize images

--- codeigniter/application/views/public_website/who_we_are_page.php
+++ codeigniter/application/views/public_website/who_we_are_page.php
@@ -4,8 +4,9 @@
 	<div class="container">
 		<div class="row row-padding">
 			<p><b>The Dynaslope Project is a research program developing an early warning system for deep-seated and catastrophic landslides, through landslide sensor technology and community participation in the Philippines</b></p>
-
+			<br>
 			<p><b>Formerly called "DEWS-L" and "DRMS" the Dynaslope Project began in the University of the Philippies Diliman and was funded by the Department of Science and Technology. Today It is implemented by the Philippine Institute of Volcanology and Seismology in 50 sites around the Philippines</b></p>
+			<br>
 		</div>
 		<br>
 		<div class="row row-padding">
@@ -21,8 +22,9 @@
 		</div>
 		<br><br>
 		<div class="row row-padding">
+			<br>
 			<p><b>In 2006, after weeks of continuous rain and an earthquake, a catastrophic landslide occured in Brgy. Guinsaugon, St. Bernard, Southern Leyte. The entire village of Guinsaugon was buried, killing over a thousand people, and destroying homes and livelihood.</b></p>
-
+			<br>
 			<p><b>The Guinsaugon Tragedy inspired the research beginnings of an early warning system for landslides. From 11 sites, the research expanded to sites in Luzon, Visayas, and Mindanao, each with an installed landslide sensor and an organized landslide early warning committee.</b></p>
 		</div>
 	</div>
@@ -35,6 +37,7 @@
 	<br><br>
 	<div class="container">
 		<div class="row row-padding">
+			<br><br>
 			<p><b>The early warning system for landslides follows a protocol that derives early warning information from sensor data and surficial ground data.</b></p>
 			<p><b>Landslide sensors are a Filipino-made, solar-powered, early warning instrument that is able to detect and monitor ground movement, soil moisture, and is equipped with a rain gauge. Sensors send data to the PHIVOLCS headquarters every 30 minutes, 24 hours-7 days a week.</b></p><br>
 			<p><b>The LEWC is composed of members of the community at risk of landslide. They are the projects's partners on-the-ground who participate in learning about the landslide risk, monitoring and warning through measuring observation markers for ground movement, disseminating and communicating early warning information, and response.</b></p>
@@ -54,12 +57,15 @@
 			<p><b>Envisioning disaster-resilient communities begins with disaster preparedness. Early warning systems help ensure that the vulnerable are safe even before a disasster strikes. By working together and learning from one another, the Dynaslope project moves forward towards this goal.</b></p>
 			<br>
 		</div>
-		<br><br>
+		<br>
 		<div class="row row-padding video-container">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/FdGrkoWxYS0" frameborder="0" allowfullscreen></iframe>
+			<iframe width="700" height="455" src="https://www.youtube.com/embed/FdGrkoWxYS0" frameborder="0" allowfullscreen></iframe>
+			<br><br><br>
 		</div>
-		<div class="row stylish-typeface">
-			
+		<div class="row row-padding stylish-typeface">
+			<p>"Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Vivamus quis porta ipsum, nec mattis dolor. Aliquam euismod tempor laoreet."</p>
+			<br>
+			<p style="text-align: right;">- Director Renato U. Solidum, Jr.</p>
 		</div>
 	</div>
 </div>
