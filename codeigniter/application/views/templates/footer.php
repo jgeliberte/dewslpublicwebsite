13421a8032b5009eac00b5d899d68de7953f9b3b
John Geliberte <gelibertejohn@gmail.com>
2017-10-27 07:49:51 +0800

    Added ref link to phivolcs website

--- codeigniter/application/views/templates/footer.php
+++ codeigniter/application/views/templates/footer.php
@@ -4,9 +4,9 @@
 	<nav class="navbar" role="navigation">
 		<div class="container">
 			<div class="navbar-text text-center col-sm-12">
-				<div>4F PHIVOLCS Building, C. P. Garcia Avenue, UP Campus, Diliman, Quezon City 1011</div>
+				<a href="http://www.phivolcs.dost.gov.ph/"><div>4F PHIVOLCS Building, C. P. Garcia Avenue, UP Campus, Diliman, Quezon City 1011</div>
 				<div>Tel. no. (02) 426-1458 loc. 152 to 153 | dynaslope.phivolcs@gmail.com</div>
-				<div>www.facebook.com/dynaslopecommunity</div>
+				<div>www.facebook.com/dynaslopecommunity</div></a>
 			</div>
 		</div>
 	</nav>
