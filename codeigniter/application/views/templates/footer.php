01114099770cc2345e2279f4c8a5e23b982634c2
John Geliberte <gelibertejohn@gmail.com>
2017-10-27 09:18:47 +0800

    Change link to dynacommunity fb page

--- codeigniter/application/views/templates/footer.php
+++ codeigniter/application/views/templates/footer.php
@@ -5,8 +5,8 @@
 		<div class="container">
 			<div class="navbar-text text-center col-sm-12">
 				<a href="http://www.phivolcs.dost.gov.ph/"><div>4F PHIVOLCS Building, C. P. Garcia Avenue, UP Campus, Diliman, Quezon City 1011</div>
-				<div>Tel. no. (02) 426-1458 loc. 152 to 153 | dynaslope.phivolcs@gmail.com</div>
-				<div>www.facebook.com/dynaslopecommunity</div></a>
+				<div>Tel. no. (02) 426-1458 loc. 152 to 153 | dynaslope.phivolcs@gmail.com</div></a>
+				<a href="http://www.facebook.com/dynaslopecommunity"><div>www.facebook.com/dynaslopecommunity</div></a>
 			</div>
 		</div>
 	</nav>
