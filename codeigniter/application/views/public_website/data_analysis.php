3cfab3bafd04b6466b0a4ae4ae82f48cc03ba2ff
ivy jean dilig <diligivyjean@gmail.com>
2017-11-24 18:07:04 +0800

    - update analysis_graphs

--- codeigniter/application/views/public_website/data_analysis.php
+++ codeigniter/application/views/public_website/data_analysis.php
@@ -12,17 +12,8 @@
 <script src="/js/dewslandslide/public_website/analysis_graph.js"></script>
 
 <style type="text/css">
-    .rainGraph { margin-top: 10px; }
-    .highcharts-container { margin: auto; }
-    .range-picker {
-        background: #fff; 
-        cursor: pointer; 
-        padding: 5px 10px; 
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        margin: 5px 10px 10px; 0; 
-        width: 25%
-    }
+.rainGraph { margin-top: 10px; }
+.highcharts-container { margin: auto; }
 </style>
 
 <div class="row">
@@ -31,20 +22,32 @@
             <span class="circle left"></span>
             <div class="container-line-text section-head-text">
                 Surficial Graph
+
             </div>
             <span class="circle right"></span>
         </div>
     </div>
 </div>
-
+<div id="with_data"></div>
 <div class="row">
     <div class="col-sm-12">
         <div class="alert alert-warning">
             <strong>NOTE:</strong> Surficial data is the data from the ground marker measurements sent by the LEWC (Landslide Early Warning Committee). The measurements between two markers are plotted on the y-axis with the date/time of the measurement on the x-axis.
         </div>
-
-        <div id="reportrangesurficial" class="col-sm-8 pull-right range-picker">
-            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;<strong class="caret"></strong>
+        <div class="container">
+            <div class='col-md-4 pull-right'>
+                <div class="form-group">
+                    <div class='input-group date' id='datetimepicker7'>
+                        <div class="input-group-btn load_surficial" id="div_load_surficial" >
+                            <button type="button" class="btn btn-primary" id="load_surficial">Load Graph</button>
+                        </div>
+                        <input id="reportrangesurficial" type='text' class="form-control pull-right text-center" />
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-calendar"></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -71,9 +74,20 @@
         <div class="alert alert-warning">
             <strong>NOTE:</strong> Rainfall values are represented as either the 24-hour cumulative value or the 72-hour cumulative value. The date and time is represented on the x-axis while the rainfall values are on the y-axis. Threshold values are indicated as horizontal lines on the graph. The 72-hour cumulative rainfall threshold is based on the 2-year rainfall maximum, while the 24-hour cumulative rainfall threshold is based on half the aforementioned rainfall maximum.
         </div>
-
-        <div id="reportrangerain" class="col-sm-8 pull-right range-picker">
-            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;<strong class="caret"></strong>
+        <div class="container">
+            <div class='col-md-4 pull-right'>
+                <div class="form-group">
+                    <div class='input-group date' id='datetimepicker7'>
+                        <div class="input-group-btn load_rain"  id="div_load_rain">
+                            <button type="button" class="btn btn-primary" id="load_rain">Load Graph</button>
+                        </div>
+                        <input id="reportrangerain" type='text' class="form-control pull-right text-center" />
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-calendar"></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -100,9 +114,20 @@
         <div class="alert alert-warning">
             <strong>NOTE:</strong> Column position plots are the graphic representation of the column’s position underground. Nodes are labeled from 1 onwards, where node 1 represents the topmost node. The downslope direction is parallel to the dip of the of the landslide mass, while the across-slope direction is perpendicular to the downslope. A positive value for the downslope direction indicates the top of a particular node moving towards the slope (and vice versa); and a positive value for the across-slope direction indicates the top of the node moving to the right (and vice versa).
         </div>
-
-        <div id="reportrangesubsurface" class="col-sm-8 pull-right range-picker">
-            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;<strong class="caret"></strong>
+        <div class="container">
+            <div class='col-md-4 pull-right'>
+                <div class="form-group">
+                    <div class='input-group date' id='datetimepicker7'>
+                        <div class="input-group-btn load_subsurface" id="div_load_subsurface">
+                            <button type="button" class="btn btn-primary load_subsurface" id="load_subsurface">Load Graph</button>
+                        </div>
+                        <input id="reportrangesubsurface" type='text' class="form-control pull-right text-center" />
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-calendar"></span>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -123,4 +148,3 @@
         </div>
     </div>
 </div>
-        
